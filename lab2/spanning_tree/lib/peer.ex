9f41143986915658f9f0dc3d04fa60e33015bd9a
ad5915 <dan.alex97@yahoo.com>
2018-01-26 16:28:36 +0000

    Fixed multicast tree.

--- lab2/spanning_tree/lib/peer.ex
+++ lab2/spanning_tree/lib/peer.ex
@@ -1,6 +1,6 @@
 defmodule Peer do
-  def build_tree(peers, msg, children, parent) do
-    {parent, children} = receive do
+  def build_tree(peers, msg, children, acks, parent) do
+    {parent, children, acks} = receive do
       { :parent, rec_parent } ->
         if parent == nil do
           IO.puts ["#{inspect self()} > parent: " , inspect rec_parent]
@@ -10,24 +10,32 @@ defmodule Peer do
           end)
 
           send rec_parent, { :ack, self() }
-          {rec_parent, children}
+          {rec_parent, children, acks}
         else
           send rec_parent, { :nack, self() }
-          {parent, children}
+          {parent, children, acks}
         end
       { :ack, child } ->
         children = children ++ [child]
         IO.puts ["#{inspect self()} > children:", inspect children]
-        {parent, children}
+        {parent, children, acks + 1}
 
       { :nack, _child } ->
-        {parent, children}
+        {parent, children, acks + 1}
+        #IO.puts ["#{inspect self()} > nack from ", inspect child]
+
     after
-      1_000 ->
-        {parent, children}
+      10_000 ->
+        {parent, children, acks}
     end
 
-    build_tree(peers, msg + 1, children, parent)
+    # IO.puts length(peers)
+    # IO.puts inspect acks
+    if acks < length(peers) do
+      build_tree(peers, msg + 1, children, acks, parent)
+    else
+      {parent, children, acks}
+    end
   end
 
   def delagate(value, children, parent) do
@@ -58,9 +66,9 @@ defmodule Peer do
     receive do
       {:bind, peers} ->
         IO.puts ["#{inspect self()} > bind"]
-        {parent, children} = build_tree(peers, 0, [], nil)
+        {parent, children, _} = build_tree(peers, 0, [], 0, nil)
 
-        :timer.sleep(:timer.seconds(1))
+        :timer.sleep(:timer.seconds(10))
         IO.puts ["#{inspect self()} > multicast tree built: ", inspect {parent, children}]
 
         :timer.sleep(:timer.seconds(1))
